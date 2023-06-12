cd83991e28b38ff41c78e79d0f62b660cc2139a8
camnhung1 <nbui20256@gmail.com>
2023-06-12 13:13:55 +0700

    s

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-@25ou_^i4c(_fxkx-nuo0oizwk-a9+n8---66%*x()^%3f3+!w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
